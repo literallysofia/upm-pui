a1bba235dbdee7e84387085be9b108c291918533
Sofia Silva <up201505628@fe.up.pt>
2019-10-23 19:01:53 +0200

    added toolbard and styles

--- android-app/app/src/main/java/com/example/newsmanager/MainActivity.java
+++ android-app/app/src/main/java/com/example/newsmanager/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.newsmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
@@ -30,6 +32,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         LongOperation runningTask;
         runningTask = new LongOperation();
         runningTask.execute();
