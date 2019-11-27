8c09572608e92d2af254dbde72982c0e7f5ca78a
Sofia Silva <up201505628@fe.up.pt>
2019-11-27 16:21:41 +0100

    minor commit

--- desktop-app/src/application/ArticleDetailsController.java
+++ desktop-app/src/application/ArticleDetailsController.java
@@ -3,37 +3,55 @@
  */
 package application;
 
+import java.io.IOException;
 
 import application.news.Article;
 import application.news.User;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
 
 /**
  * @author ÁngelLucas
  *
  */
 public class ArticleDetailsController {
-	//TODO add attributes and methods as needed
-	    private User usr;
-	    private Article article;
+	// TODO add attributes and methods as needed
+	private User usr;
+	private Article article;
 
-	    
+	@FXML
+	private Button backButton;
 
-		/**
-		 * @param usr the usr to set
-		 */
-		void setUsr(User usr) {
-			this.usr = usr;
-			if (usr == null) {
-				return; //Not logged user
+	/**
+	 * @param usr the usr to set
+	 */
+	void setUsr(User usr) {
+		this.usr = usr;
+		if (usr == null) {
+			return; // Not logged user
+		}
+		// TODO Update UI information
+	}
+
+	/**
+	 * @param article the article to set
+	 */
+	void setArticle(Article article) {
+		this.article = article;
+		// TODO complete this method
+	}
+
+	@FXML
+	void initialize() {
+		assert backButton != null : "fx:id=\"backButton\" was not injected: check your FXML file 'ArticleDetails.fxml'.";
+		backButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent e) {
+				// TODO go back to the other scene
 			}
-			//TODO Update UI information
-		}
-
-		/**
-		 * @param article the article to set
-		 */
-		void setArticle(Article article) {
-			this.article = article;
-			//TODO complete this method
-		}
+		});
+	}
 }
