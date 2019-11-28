c3104f425c16ef3a1d9f52c9f3849194e9c3b08f
Daniel Machado <daniel.pmachado@hotmail.com>
2019-11-28 16:46:14 +0000

    login error message

--- desktop-app/src/application/LoginController.java
+++ desktop-app/src/application/LoginController.java
@@ -1,6 +1,9 @@
 package application;
 
 import java.io.IOException;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import application.news.User;
 import javafx.event.ActionEvent;
@@ -10,7 +13,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
 import serverConection.ConnectionManager;
+import serverConection.exceptions.AuthenticationError;
 
 public class LoginController {
 //TODO Add all attribute and methods as needed 
@@ -27,7 +32,9 @@ public class LoginController {
 	private TextField userField;
 	@FXML
 	private TextField passwordField;
-
+	@FXML
+	private Text loginError;
+	
 	public LoginController() {
 
 		// Uncomment next sentence to use data from server instead dummy data
@@ -53,26 +60,33 @@ public class LoginController {
 				try {				
 					 username = userField.getText().toString();
 			         password = passwordField.getText().toString();
-			         
+			        	         
 			         User usr;		         
 			         usr = loginModel.validateUser(username, password);
-			         if(usr == null)
+			         if(usr == null) {
 			        	 System.out.print("credentials do not match");
-			         else {
-			        	
-			        	loggedUsr = usr;
+			         	 loginError.setText("These credentials do not match");
+			         }else {		        	
+			        	loggedUsr = usr;						
+						Properties prop = new Properties();
+						prop.setProperty(ConnectionManager.ATTR_SERVICE_URL, "https://sanger.dia.fi.upm.es/pui-rest-news/");
+						prop.setProperty(ConnectionManager.ATTR_REQUIRE_SELF_CERT, "TRUE");
 						FXMLLoader loader = new FXMLLoader(getClass().getResource(AppScenes.READER.getFxmlFile()));
 						loader.load();
-						NewsReaderController controller = loader.<NewsReaderController>getController();
+						NewsReaderController controller = loader.<NewsReaderController>getController();			
+						ConnectionManager connection = new ConnectionManager(prop);
+						connection.setAnonymousAPIKey("DEV_TEAM_48392");
+						controller.setConnectionManager(connection);
+						controller.setUsr(loggedUsr);
 						loginButton.getScene().setRoot(loader.getRoot());
 					}
 					
 				} catch (IOException e1) {
 					e1.printStackTrace();
+				}catch (AuthenticationError e2) {
+					Logger.getGlobal().log(Level.SEVERE, "Error in loging process");
+					e2.printStackTrace();	
 				}	
-				
-				
-
 			}
 		});
 	}
