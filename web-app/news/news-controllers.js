10f451304c457ee3212cd3072aafde75e9c7751a
Daniel Machado <daniel.pmachado@hotmail.com>
2019-12-12 13:14:27 +0000

    deleting articles without image

--- web-app/news/news-controllers.js
+++ web-app/news/news-controllers.js
@@ -2,7 +2,7 @@
 /////////////////////////////////////// NEWS CONTROLLERS /////////////////////////////////////////
 *************************************************************************************************/
 
-app.controller("newsController", function ($scope, NewsListService) {
+app.controller("newsController", function ($scope, $location, $window, NewsListService, NewsDetailsService) {
 
     $scope.getArticles = function () {
         NewsListService.query({}, function (data) {
@@ -17,11 +17,35 @@ app.controller("newsController", function ($scope, NewsListService) {
             })
     };
 
+    $scope.deleteArticle = function(article){
+        
+        console.log("Delete article: " + article.title);
+
+        console.log(article);
+       
+        if (confirm('The article: "' + article.title + '" is going to be removed. Are you sure?')) {
+            
+            NewsDetailsService.delete(article, 
+               
+                function(data){
+                    console.log(data);
+                   
+                },
+                function (error) {
+                    console.log("There was an error deleting.");
+                    console.log(error);
+                    $window.alert("There was an error deleting the news: " + error.statusText);
+                })
+        }
+
+        $location.path("/");
+    };
+
     $scope.getArticles();
 });
 
 
-app.controller("newsCreationCtrl", function ($scope, NewsDetailsService) {
+app.controller("newsCreationCtrl", function ($scope, $window, $location, NewsDetailsService) {
 
     $scope.addNews = function(article){
         article.img
@@ -35,6 +59,7 @@ app.controller("newsCreationCtrl", function ($scope, NewsDetailsService) {
             $window.alert("There was an error loading the news: " + error.statusText);
         });
 
+        $location.path("/");
         
     };
 
