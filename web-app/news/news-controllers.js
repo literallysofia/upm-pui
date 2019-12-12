79f917048d8f2f4d80fd237cc9b405765830dac3
Sofia Silva <up201505628@fe.up.pt>
2019-12-12 14:04:22 +0100

    login (with validation) :white_check_mark:

--- web-app/news/news-controllers.js
+++ web-app/news/news-controllers.js
@@ -20,22 +20,42 @@ app.controller("newsController", function ($scope, NewsListService) {
     $scope.getArticles();
 });
 
+app.controller("loginController", function ($scope, $http, $location, LoginService) {
+
+    $scope.login = function () {
+        LoginService.login({
+                passwd: $scope.login.password,
+                username: $scope.login.username
+            }, function (loginres) {
+                $http.defaults.headers.common['Authorization'] = loginres.Authorization + ' apikey=' + loginres.apikey;
+                $scope.login = {};
+                $scope.loginForm.$setPristine();
+                console.log("Successful login!");
+                console.log(loginres);
+                $location.path('/');
+            },
+            function (error) {
+                console.log("There was an error with the login.");
+                console.log(error);
+                $scope.login.error = true;
+            })
+    };
+
+});
 
 app.controller("newsCreationCtrl", function ($scope, NewsDetailsService) {
 
-    $scope.addNews = function(article){
+    $scope.addNews = function (article) {
         article.img
-        NewsDetailsService.save(article, 
-        function(data){
-            console.log(data);
-        },
-        function (error) {
-            console.log("There was an error loading the news.");
-            console.log(error);
-            $window.alert("There was an error loading the news: " + error.statusText);
-        });
-
-        
+        NewsDetailsService.save(article,
+            function (data) {
+                console.log(data);
+            },
+            function (error) {
+                console.log("There was an error loading the news.");
+                console.log(error);
+                $window.alert("There was an error loading the news: " + error.statusText);
+            });
     };
 
 });
