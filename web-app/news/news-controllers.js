f666c3f92ec1ada024d6221f5580b3bd56daa949
Sofia Silva <up201505628@fe.up.pt>
2019-12-15 19:41:17 +0100

    delete article and user confirmation :white_check_mark:

--- web-app/news/news-controllers.js
+++ web-app/news/news-controllers.js
@@ -5,6 +5,9 @@
 app.controller("NewsController", function ($scope, $rootScope, $window, $location, NewsListService, NewsDetailsService) {
 
     $rootScope.currentPath = $location.path();
+    $scope.articleToDelete = {};
+    $scope.showModal = false;
+    $scope.showAlert = false;
 
     $scope.getArticles = function () {
         NewsListService.query({}, function (data) {
@@ -19,26 +22,31 @@ app.controller("NewsController", function ($scope, $rootScope, $window, $locatio
             })
     };
 
-    $scope.deleteArticle = function (article) {
-        console.log("Delete article: " + article.title);
-        console.log(article);
+    $scope.getArticles();
 
-        if (confirm('The article: "' + article.title + '" is going to be removed. Are you sure?')) {
-            NewsDetailsService.delete(article,
-                function (data) {
-                    console.log(data);
-                    console.log("ja deu!");
-                },
-                function (error) {
-                    console.log("There was an error deleting.");
-                    console.log(error);
-                    $window.alert("There was an error deleting the news: " + error.statusText);
-                })
-        }
-        //$location.path("/");
+    $scope.toggleModal = function (article) {
+        $scope.articleToDelete = article;
+        $scope.showModal = !$scope.showModal;
     };
 
-    $scope.getArticles();
+    $scope.deleteArticle = function () {
+        console.log("Deleting article...");
+        NewsDetailsService.delete({
+                id: $scope.articleToDelete.id
+            },
+            function (data) {
+                console.log(data);
+                console.log("Article deleted!");
+                $scope.showAlert = true;
+                $scope.getArticles();
+            },
+            function (error) {
+                console.log("There was an error when trying to delete the article.");
+                console.log(error);
+                $window.alert("There was an error when trying to delete the article: " + error.statusText);
+            })
+    };
+
 });
 
 app.controller("LoginController", function ($scope, $rootScope, $http, $location, LoginService) {
@@ -154,4 +162,55 @@ app.controller("CreateArticleController", function ($scope, $rootScope, $window,
         $location.path("/");
     };
 
+});
+
+app.directive('modal', function () {
+    return {
+        template: '<div class="modal fade">' +
+            '<div class="modal-dialog modal-dialog-centered" role="document">' +
+            '<div class="modal-content">' +
+            '<div class="modal-header">' +
+            '<h5 class="modal-title" id="modalCenterTitle">Are you sure?</h5>' +
+            '<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>' +
+            '</div>' +
+            '<div class="modal-body">' +
+            'Do you really want to delete the article "{{articleToDelete.title}}"? <br> This process cannot be undone.' +
+            '</div>' +
+            '<div class="modal-footer">' +
+            '<button type="button" class="btn btn-dark" data-dismiss="modal">Cancel</button>' +
+            '<button type="button" class="btn btn-danger" ng-click="delete()">Delete</button>' +
+            '</div>' +
+            '</div>' +
+            '</div>' +
+            '</div>',
+        restrict: 'E',
+        transclude: true,
+        replace: true,
+        scope: true,
+        link: function postLink(scope, element, attrs) {
+            scope.$watch(attrs.visible, function (value) {
+                if (value == true)
+                    $(element).modal('show');
+                else
+                    $(element).modal('hide');
+            });
+
+            scope.delete = function () {
+                scope.deleteArticle();
+                $(element).modal('hide');
+            };
+
+            $(element).on('shown.bs.modal', function () {
+                scope.$apply(function () {
+                    scope.$parent[attrs.visible] = true;
+                });
+            });
+
+            $(element).on('hidden.bs.modal', function () {
+                scope.$apply(function () {
+                    scope.$parent[attrs.visible] = false;
+                });
+            });
+        }
+    };
 });
